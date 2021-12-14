a854f9f939b40871bd80d88692f8bfd1a8a4fb0f
Dan-oss <contiv.ru@gmail.com>
2021-12-14 16:04:13 +0600

    f

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -9,41 +9,36 @@ const returnName = () => {
   console.log(`Hello ${userName}!`);
 };
 
-const getArrProgression = () => {
-  const arr1 = [];
-  const arrLength = Math.floor(Math.random() * (10 - 6)) + 6;
-  const begin = Math.floor(Math.random() * (5 - 1)) + 1;
-  const step = Math.floor(Math.random() * (5 - 2)) + 2;
-  for (let i = begin; arr1.length < arrLength; i += step) {
-    arr1.push(i);
-  }
-  return arr1; // возвращаем массив чисел в арифметической прогрессии
-};
-const arrProgression = getArrProgression();
-console.log('point1  '+ arrProgression);
 
-const positionInArr = () => Math.floor(Math.random() * 8); // возвращаем произвольную позицию в массиве
-const position = positionInArr();
-console.log('point2  '+ position);
-
-const hideValue = (position, arrProgression) => {
-  const hiddenPosition = arrProgression;
-  hiddenPosition[position] = "..";
-  return hiddenPosition.join(" "); // заменяем число в массиве символами "**" и возвращаем строку
-};
-
-console.log('point111  '+ arrProgression);
-const strHiddenPosition = hideValue(position, arrProgression);
-console.log('point3  '+ strHiddenPosition);
-console.log('point222  '+ arrProgression);
-console.log('point4  '+ arrProgression[position]);
 
 const progression = () => {
   let iter = false;
 
   for (let i = 0; i < 3; i += 1) {
+    const getArrProgression = () => {
+      const arr1 = [];
+      const arrLength = Math.floor(Math.random() * (10 - 6)) + 6;
+      const begin = Math.floor(Math.random() * (5 - 1)) + 1;
+      const step = Math.floor(Math.random() * (5 - 2)) + 2;
+      for (let i = begin; arr1.length < arrLength; i += step) {
+        arr1.push(i);
+      }
+      return arr1; // возвращаем массив чисел в арифметической прогрессии
+    };
+    const arrProgression = getArrProgression();
+    
+    const positionInArr = () => Math.floor(Math.random() * 8); // возвращаем произвольную позицию в массиве
+    const position = positionInArr();
+    
+    const hideValue = (position, arrProgression) => {
+      const hiddenPosition = arrProgression.slice();
+      hiddenPosition[position] = "..";
+      return hiddenPosition.join(" "); // заменяем число в массиве символами "**" и возвращаем строку
+    };
+    
+    const strHiddenPosition = hideValue(position, arrProgression);
     console.log(`Question: ${strHiddenPosition} `);
-    const answer = readlineSync.question("Your answer: ");
+    const answer = Number(readlineSync.question(`Your answer: `));
     if (answer === arrProgression[position]) {
       iter = true;
       console.log("Correct");
@@ -56,7 +51,7 @@ const progression = () => {
       break;
     }
   }
-  if (iter = true) {
+  if (iter === true) {
     console.log(`Congratulations, ${userName}!`);
   }
 };
