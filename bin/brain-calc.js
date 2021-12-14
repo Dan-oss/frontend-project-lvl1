b97a9e8af90ed91a85cf108ac71552ef957bc0ab
Dan-oss <contiv.ru@gmail.com>
2021-12-14 15:10:35 +0600

    f

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,43 +1,42 @@
 #!/usr/bin/env node
 //import {} from '../src/even.js';
 //import returnName from '../src/cli.js';
-import readlineSync from 'readline-sync';
-const userName = readlineSync.question('May I have your name? ');
+import readlineSync from "readline-sync";
+const userName = readlineSync.question("May I have your name? ");
 const returnName = () => {
-    console.log('Welcome to the Brain Games!');
-    userName;
-    console.log(`Hello ${userName}!`);
-  };
-  
+  console.log("Welcome to the Brain Games!");
+  userName;
+  console.log(`Hello ${userName}!`);
+};
+
 returnName();
-console.log('What is the result of the expression?');
+console.log("What is the result of the expression?");
 
 const brainCalc = () => {
-    let iter = false;
+  let iter = false;
 
-    for(let i = 0; i < 3; i += 1) {
-        let num1 = Math.floor(Math.random() * 10);
-        let num2 = Math.floor(Math.random() * 10);
-        const sum = num1 + num2;
-        console.log (`Question: ${num1} + ${num2} `);
-        const answer = Number(readlineSync.question(`Your answer: ` ));
+  for (let i = 0; i < 3; i += 1) {
+    let num1 = Math.floor(Math.random() * 10);
+    let num2 = Math.floor(Math.random() * 10);
+    const sum = num1 + num2;
+    console.log(`Question: ${num1} + ${num2} `);
+    const answer = Number(readlineSync.question(`Your answer: `));
     if (answer === sum) {
-        iter = true;
-        console.log('Correct');
+      iter = true;
+      console.log("Correct");
     }
-    if (answer !== sum) { console.log(`${answer} is wrong answer ;(. Correct answer was ${sum}. \nLet's try again, ${userName}!`);
-        iter = false;
-        break;
-        }
+    if (answer !== sum) {
+      console.log(
+        `${answer} is wrong answer ;(. Correct answer was ${sum}. \nLet's try again, ${userName}!`
+      );
+      iter = false;
+      break;
     }
-    
+  }
+
   if (iter === true) {
-      console.log(`Congratulations, ${userName}!`);
+    console.log(`Congratulations, ${userName}!`);
   }
 };
-    
-
-
 
 console.log(brainCalc());
-
--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,48 +1,66 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
-const userName = readlineSync.question('May I have your name? ');
+import readlineSync from "readline-sync";
+
+const userName = readlineSync.question("May I have your name? ");
 const returnName = () => {
-    console.log('Welcome to the Brain Games!');
-    userName;
-    console.log(`Hello ${userName}!`);
-  };
+  console.log("Welcome to the Brain Games!");
+  userName;
+  console.log(`Hello ${userName}!`);
+};
 
+const getArrProgression = () => {
+  const arr1 = [];
+  const arrLength = Math.floor(Math.random() * (10 - 6)) + 6;
+  const begin = Math.floor(Math.random() * (5 - 1)) + 1;
+  const step = Math.floor(Math.random() * (5 - 2)) + 2;
+  for (let i = begin; arr1.length < arrLength; i += step) {
+    arr1.push(i);
+  }
+  return arr1; // возвращаем массив чисел в арифметической прогрессии
+};
+const arrProgression = getArrProgression();
+console.log('point1  '+ arrProgression);
 
+const positionInArr = () => Math.floor(Math.random() * 8); // возвращаем произвольную позицию в массиве
+const position = positionInArr();
+console.log('point2  '+ position);
 
+const hideValue = (position, arrProgression) => {
+  const hiddenPosition = arrProgression;
+  hiddenPosition[position] = "..";
+  return hiddenPosition.join(" "); // заменяем число в массиве символами "**" и возвращаем строку
+};
 
-const progression = (arr) => {
-    let iter = false;
-    const getArrProgression = () => {
-        const arr = [];
-        const arrLength = Math.floor(Math.random() * (10 - 6)) + 6;
-        let begin = Math.floor(Math.random() * (5 - 1)) + 1;
-        let step = Math.floor(Math.random() * (5 - 2)) + 2;
-        for (let i = begin; arr.length < arrLength; i += step) {
-          arr.push(i);
-        };
-        return arr;
-      };
-    const hideValue = (arr) => {
-        const positionInArr = Math.floor(Math.random() * arr.length);
-        arr[positionInArr] = '..';
-        return arr.join(' ');
-        };
+console.log('point111  '+ arrProgression);
+const strHiddenPosition = hideValue(position, arrProgression);
+console.log('point3  '+ strHiddenPosition);
+console.log('point222  '+ arrProgression);
+console.log('point4  '+ arrProgression[position]);
 
-    for(let i = 0; i < 3; i += 1) {
-        console.log (`Question: ${hideValue(getArrProgression)} `);
-        const answer = readlineSync.question(`Your answer: ` );
-        if (answer === arr[positionInArr]) {
-            iter = true;
-            console.log('Correct');
-        }
-        if (answer !== arr[positionInArr]) { console.log(`${answer} is wrong answer ;(. Correct answer was ${arr[positionInArr]}. \nLet's try again, ${userName}!`);
-            iter = false;
-            break;
-            }
-  };
-}
+const progression = () => {
+  let iter = false;
 
-  returnName();
-  console.log('What number is missing in the progression?');
-  progression();
+  for (let i = 0; i < 3; i += 1) {
+    console.log(`Question: ${strHiddenPosition} `);
+    const answer = readlineSync.question("Your answer: ");
+    if (answer === arrProgression[position]) {
+      iter = true;
+      console.log("Correct");
+    }
+    if (answer !== arrProgression[position]) {
+      console.log(
+        `${answer} is wrong answer ;(. Correct answer was ${arrProgression[position]}. \nLet's try again, ${userName}!`
+      );
+      iter = false;
+      break;
+    }
+  }
+  if (iter = true) {
+    console.log(`Congratulations, ${userName}!`);
+  }
+};
+
+returnName();
+console.log("What number is missing in the progression?");
+progression();
