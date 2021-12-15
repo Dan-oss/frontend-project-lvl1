b901df5cce2a0296c5245756df2d890dbd548b53
Dan-oss <contiv.ru@gmail.com>
2021-12-15 11:40:05 +0600

    make russia great again

--- src/even.js
+++ src/even.js
@@ -3,8 +3,9 @@ import readlineSync from 'readline-sync';
 export const returnName = readlineSync.question('May I have your name? ');
 export const getRandomNum = Math.floor(Math.random() * 100);
 export const returnAnswer = readlineSync.question(getRandomNum);
-if (getRandomNum % 2 === 0 && returnAnswer === 'yes' || getRandomNum % 2 !== 0 && returnAnswer === 'no' ) {
+// eslint-disable-next-line no-mixed-operators
+if (getRandomNum % 2 === 0 && returnAnswer === 'yes' || getRandomNum % 2 !== 0 && returnAnswer === 'no') {
   console.log('Correct!');
-}
-else
+} else {
   console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.Let's try again, ${returnName}!`);
+}
