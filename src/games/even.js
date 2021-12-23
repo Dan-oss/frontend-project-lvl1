43e2a2b91579f873f2eabd86cc54a96740540d20
Dan-oss <contiv.ru@gmail.com>
2021-12-23 12:17:22 +0600

    new func isEven to even.js

--- src/games/even.js
+++ src/games/even.js
@@ -4,8 +4,8 @@ export const getGameData = () => {
   const max = 10;
   const min = 1;
   const num = randomNum(max, min);
-
-  const answer = num % 2 === 0 ? 'yes' : 'no';
+  const isEven = (number) => number % 2 === 0;
+  const answer = isEven(num) ? 'yes' : 'no';
   const question = `Question: ${num}`;
   const data = [question, answer];
   return data;
