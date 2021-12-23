18a61f8368b9df83762cf756f09b6dc83dfc02f4
Dan-oss <contiv.ru@gmail.com>
2021-12-23 11:35:01 +0600

    calc operation

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,12 +1,29 @@
 import { randomNum } from '../cli.js';
 
+const getRandomSymbol = () => {
+  const arr = ['+', '-', '*'];
+  const random = arr[randomNum(0, arr.length - 1)];
+  return random;
+};
+
 export const getGameData = () => {
+  const randomSymbol = getRandomSymbol();
   const max = 10;
   const min = 1;
   const num1 = randomNum(max, min);
   const num2 = randomNum(max, min);
-  const answer = String(num1 + num2);
-  const question = `Question: ${num1} + ${num2} `;
+  const getAnswer = () => {
+    let result = 0;
+    if (randomSymbol === '*') {
+      result = num1 * num2;
+    }
+    if (randomSymbol === '-') {
+      result = num1 - num2;
+    } else result = num1 + num2;
+    return String(result);
+  };
+  const answer = getAnswer();
+  const question = `Question: ${num1} ${randomSymbol} ${num2} `;
   const arr = [question, answer];
   return arr;
 };
