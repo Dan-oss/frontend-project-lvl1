3b413aa5b80b78edf88c1ac32ac66d3fdea1d0ae
Dan-oss <contiv.ru@gmail.com>
2021-12-28 11:09:12 +0600

    fixed getAnswer from calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -6,6 +6,16 @@ const getRandomSymbol = () => {
   const random = arrOfSymbols[randomNum(0, arrOfSymbols.length - 1)];
   return random;
 };
+const getAnswer = (num1, num2, randomSymbol) => {
+  let result = 0;
+  if (randomSymbol === '*') {
+    result = num1 * num2;
+  }
+  if (randomSymbol === '-') {
+    result = num1 - num2;
+  } else result = num1 + num2;
+  return String(result);
+};
 
 const getGameData = () => {
   const randomSymbol = getRandomSymbol();
@@ -13,23 +23,14 @@ const getGameData = () => {
   const max = 10;
   const num1 = randomNum(min, max);
   const num2 = randomNum(min, max);
-  const getAnswer = () => {
-    let result = 0;
-    if (randomSymbol === '*') {
-      result = num1 * num2;
-    }
-    if (randomSymbol === '-') {
-      result = num1 - num2;
-    } else result = num1 + num2;
-    return String(result);
-  };
-  const answer = getAnswer();
+
+  const answer = getAnswer(num1, num2, randomSymbol);
   const question = `Question: ${num1} ${randomSymbol} ${num2} `;
   const data = [question, answer];
   return data;
 };
 const ruleGame = 'What is the result of the expression?';
 
-const calculate = () => gamePlay(getGameData, ruleGame);
+export const calculate = () => gamePlay(getGameData, ruleGame);
 
 export default calculate();
