aaa2a366a64c7edaf3ff44cab5a83c8b78ed75bb
Dan-oss <contiv.ru@gmail.com>
2022-01-01 19:35:14 +0600

    rewrite const hideValue in progressio.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,26 +1,22 @@
 import randomNum from '../utils.js';
 
+const getArrProgression = () => {
+  const progression = [];
+  const arrLength = randomNum(6, 10);
+  const begin = randomNum(1, 5);
+  const step = randomNum(2, 4);
+  for (let j = begin; progression.length < arrLength; j += step) {
+    progression.push(j);
+  }
+  return progression; // возвращаем массив чисел в арифметической прогрессии
+};
+
 export const getGameData = () => {
-  const getArrProgression = () => {
-    const progression = [];
-    const arrLength = randomNum(6, 10);
-    const begin = randomNum(1, 5);
-    const step = randomNum(2, 4);
-    for (let j = begin; progression.length < arrLength; j += step) {
-      progression.push(j);
-    }
-    return progression; // возвращаем массив чисел в арифметической прогрессии
-  };
   const arrProgression = getArrProgression();
   const positionInArr = randomNum(1, 5);
-
-  const hideValue = (position, progression) => {
-    const hiddenPosition = progression.slice();
-    hiddenPosition[position] = '..';
-    return hiddenPosition.join(' '); // заменяем число в массиве символами "**" и возвращаем строку
-  };
   const answer = String(arrProgression[positionInArr]);
-  const question = `Question: ${hideValue(positionInArr, arrProgression)} `;
+  const hideValue = arrProgression.map((elem, index) => (index === positionInArr ? '..' : elem)).join(' ');
+  const question = `Question: ${hideValue} `;
   const gameData = [question, answer];
   return gameData;
 };
