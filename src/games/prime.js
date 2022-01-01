5f15c46db059bcaa1250e8d93398c9bd218108c2
Dan-oss <contiv.ru@gmail.com>
2022-01-01 11:11:32 +0600

    rewrite func isPrime in prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,21 +1,22 @@
 import randomNum from '../utils.js';
 
+const isPrime = (num) => {
+  let result = 'yes';
+  for (let j = 2; j < num; j += 1) {
+    if (num % j === 0) {
+      result = 'no';
+      break;
+    }
+  }
+  return result;
+};
 export const getGameData = () => {
   const max = 20;
   const min = 3;
-  const num1 = randomNum(min, max);
-  const answer = () => {
-    let result = 'yes';
-    for (let j = 2; j < num1; j += 1) {
-      if (num1 % j === 0) {
-        result = 'no';
-        break;
-      }
-    }
-    return result;
-  };
-  const question = `Question: ${num1}`;
-  const gameData = [question, answer(num1)];
+  const num = randomNum(min, max);
+  const answer = isPrime(num);
+  const question = `Question: ${num}`;
+  const gameData = [question, answer];
   return gameData;
 };
 export const ruleGame = 'Answer "yes" if given number is prime. Otherwise answer "no"';
